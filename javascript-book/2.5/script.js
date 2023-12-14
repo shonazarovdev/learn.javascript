4e046d607270786ddc416f08ce17269a3d9f782f
Mukhammad <mukhammaddev9@gmail.com>
2023-12-14 09:02:05 +0500

    javascript-book: 2.5

---
+++ javascript-book/2.5/script.js
@@ -0,0 +1,70 @@
+/*
+
+ 	ПРИМИТИВНЫЕ ТИПЫ ДАННЫХ
+
+
+ 	1.Number
+ 	let n = 123
+
+ 	2.BigInt
+ 	const bigInt = 1234567890123456789012345678901234567890n
+
+ 	3.String
+ 	let str = "Привет";
+ 	let str2 = 'Одинарные кавычки тоже подойдут';
+ 	let phrase = `Обратные кавычки позволяют встраивать переменные ${str}`;
+
+	4.Boolean
+	let nameFieldChecked = true; // да, поле отмечено
+	let ageFieldChecked = false; // нет, поле не отмечено
+
+ 	5.Null
+ 	let age = null;
+
+ 	6.Undefined
+ 	let work;
+
+ 	alert(work); // выведет "undefined"
+
+ 	7.Symbol
+ 	Введен в ECMAScript 6 (ES6) и представляет уникальные и неизменяемые значения.
+
+*/
+
+/*
+
+	ОБЪЕКТНЫЕ ТИПЫ ДАННЫХ
+
+	1.Object
+	let person = {
+	  name: 'Иван',
+	  age: 25,
+	  isStudent: false
+	};
+
+	2.Array
+	let numbers = [1, 2, 3, 4, 5];
+
+	3.Function
+	function add(x, y) {
+	 	return x + y;
+	}
+
+	4.Date
+	let currentDate = new Date();
+
+	let year = currentDate.getFullYear();
+	let month = currentDate.getMonth() + 1;
+	let day = currentDate.getDate();
+	let hours = currentDate.getHours();
+	let minutes = currentDate.getMinutes();
+	let seconds = currentDate.getSeconds();
+
+	5.Regex
+	let pattern = /apple/;
+	let text = "I have an apple and a banana.";
+
+	Проверка наличия слова "apple" в тексте
+	console.log(pattern.test(text));
+
+*/
